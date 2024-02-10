ee86e06a0ec01fa878514463e94b64a1f92c9619
Morpheus <57977077+dledbetter123@users.noreply.github.com>
2024-02-10 12:04:19 -0500

    Stable development, pre-aws production building

--- backend/server.go
+++ backend/server.go
@@ -5,10 +5,26 @@ import (
 	"net/http"
 )
 
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*") // Be careful with '*', in production it's better to specify the exact origin
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	// Set the content type to plain text for simplicity
+	w.Header().Set("Content-Type", "text/plain")
+	// Respond with the status message
+	fmt.Fprintf(w, "Backend stable")
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		enableCors(&w)
 		fmt.Fprintf(w, "Hello, this is a placeholder for the portfolio backend!")
 	})
+
+	// Add the new handler function for the status route
+	http.HandleFunc("/api/status", statusHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
---
+++ frontend/src/components/IntroPage/IntroPage.js
@@ -0,0 +1,48 @@
+// src/componentns/IntroPage/IntroPage.js
+
+import React, { useEffect, useState } from 'react';
+import './IntroPage.css';
+
+import profilePic from './images/profile.jpeg'; // Update the path accordingly
+
+const IntroPage = () => {
+  const [backendStatus, setBackendStatus] = useState('loading');
+  const [hoverTitle, setHoverTitle] = useState('');
+
+  useEffect(() => {
+    // will need to use the actual IP address or domain of your backend service when in production
+    fetch('http://localhost:8080/api/status') 
+      .then(response => response.text())
+      .then(text => {
+        if (text === "Backend stable") {
+          setBackendStatus('stable');
+          setHoverTitle('Title is green! This means that the backend is successfully communicating, it would be red otherwise.');
+        } else {
+          setBackendStatus('unstable');
+          setHoverTitle('Title is Red... this means that the backend is not properly set up and communicating, it should be green.');
+        }
+      })
+      .catch(() => {
+        setBackendStatus('unstable');
+        setHoverTitle('Title is Red... this means that the backend is not properly set up and communicating, it should be green.');
+      });
+  }, []);
+
+  return (
+    <section className="introPage">
+      <div className="content">
+        <h1 style={{ fontSize: '34px' }} className={backendStatus === 'stable' ? 'accent-green' : 'accent-red'}
+         title={hoverTitle}>
+            Welcome 
+        </h1>
+        <p style={{ fontSize: '26px' }} title={hoverTitle}>I'm a software engineer with experience in Web and app development,
+          Operating Systems, as well as DevOps and DevSecOps frameworks. I'm also a machine learning
+          researcher who has developed autonomous systems and robots.</p>
+      </div>
+      <img src={profilePic} alt="Profile" className="profilePic" />
+    </section>
+  );
+};
+
+export default IntroPage;
+
